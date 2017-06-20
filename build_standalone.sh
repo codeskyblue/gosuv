627a3ac153d474c38c3be70d5a8b05a9bae7a5aa
codeskyblue <codeskyblue@gmail.com>
2017-06-20 15:58:22 +0800

    update build script

--- build_standalone.sh
+++ build_standalone.sh
@@ -5,8 +5,15 @@ set -e
 set -o pipefail
 
 cd $(dirname $0)
-source "$HOME/.bash_profile"
-export GOPATH=$GOPATH:/home/pi/build_tmp 
+
+if test $(whoami) = "pi"
+then
+    export GOPATH=/home/pi/build_tmp 
+	export GOROOT=$HOME/go
+	export PATH=$PATH:$GOROOT/bin
+fi
+#[[ -f $HOME/.bash_profile ]] && source "$HOME/.bash_profile"
+#[[ -f $HOME/.bashrc ]] && source "$HOME/.bashrc"
 
 go generate
 exec go build -tags vfs "$@"
